10823a4f12abd8c61f3abe38a6d272ffc2069028
OlehVozniuk <spirit.r4442@gmail.com>
2025-05-05 15:36:54 +0300

    update user model and router, added validator

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,30 +1,64 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined!",
-  });
+const User = require("../models/userModel");
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ status: "fail", message: err.message });
+  }
 };
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined!",
-  });
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "Користувача не знайдено" });
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ status: "fail", message: err.message });
+  }
 };
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined!",
-  });
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ status: "fail", message: err.message });
+  }
 };
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined!",
-  });
+exports.updateUser = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedUser) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "Користувача не знайдено" });
+    }
+
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(400).json({ status: "fail", message: err.message });
+  }
 };
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined!",
-  });
+exports.deleteUser = async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+
+    if (!deletedUser) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "Користувача не знайдено" });
+    }
+
+    res.status(204).json({ status: "success", data: null });
+  } catch (err) {
+    res.status(500).json({ status: "fail", message: err.message });
+  }
 };
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-// const validator = require("validator");
+const validator = require("validator");
 //name, email, photo, password, passwordConfirm
 const UserSchema = new mongoose.Schema(
   {
@@ -12,7 +12,7 @@ const UserSchema = new mongoose.Schema(
       required: [true, "A user must have an email"],
       unique: true,
       lowercase: true,
-      // validate: [validator.isEmail, "Please provide a valid email"],
+      validate: [validator.isEmail, "Please provide a valid email"],
     },
     photo: {
       type: String,
