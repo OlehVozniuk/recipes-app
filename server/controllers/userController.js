3a4755e539b89cdf0f74cf5d4c2f7e53b688be88
OlehVozniuk <spirit.r4442@gmail.com>
2025-05-27 19:29:12 +0300

    + moved patch user to user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,13 @@
 const User = require("../models/userModel");
-
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 exports.getAllUsers = async (req, res) => {
   try {
     const users = await User.find();
@@ -21,32 +29,35 @@ exports.getUser = async (req, res) => {
     res.status(500).json({ status: "fail", message: err.message });
   }
 };
-exports.createUser = async (req, res) => {
-  try {
-    const newUser = await User.create(req.body);
-    res.status(201).json(newUser);
-  } catch (err) {
-    res.status(400).json({ status: "fail", message: err.message });
-  }
-};
-exports.updateUser = async (req, res) => {
-  try {
-    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
 
-    if (!updatedUser) {
-      return res
-        .status(404)
-        .json({ status: "fail", message: "Користувача не знайдено" });
-    }
-
-    res.status(200).json(updatedUser);
-  } catch (err) {
-    res.status(400).json({ status: "fail", message: err.message });
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        "This route is not for password updates. Please use /updateMyPassword.",
+        400
+      )
+    );
   }
-};
+
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, "name", "email");
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
 exports.deleteUser = async (req, res) => {
   try {
     const deletedUser = await User.findByIdAndDelete(req.params.id);
